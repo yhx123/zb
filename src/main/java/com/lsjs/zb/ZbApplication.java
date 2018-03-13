659a7df2ffe4794cabc37ad12a678f3b9fc0b36e
yhx123 <714565396@qq.com>
2018-03-13 23:03:37 +0800

    试一下能不能推送上去

--- src/main/java/com/lsjs/zb/ZbApplication.java
+++ src/main/java/com/lsjs/zb/ZbApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * @author yhx12
+ */
 
 @EnableTransactionManagement
 @SpringBootApplication
